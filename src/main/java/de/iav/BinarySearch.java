3328bba2fc3dcd3486635da83e853397eb614bad
Houman Mohammadi <houmane.el@gmail.com>
2023-05-10 00:39:24 +0200

    final version

--- src/main/java/de/iav/BinarySearch.java
+++ src/main/java/de/iav/BinarySearch.java
@@ -1,5 +1,41 @@
 package de.iav;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BinarySearch {
 
+    public static int searchIndexOfAnArray(int[] givenArray, int elementToFindIndex) {
+
+        // Create an array to store the indices
+        Integer[] indices = new Integer[givenArray.length];
+        for (int i = 0; i < indices.length; i++) {
+            indices[i] = i;
+        }
+
+        // Sort the indices array based on the values in the original array
+        Arrays.sort(indices, new Comparator<Integer>() {
+            public int compare(Integer i1, Integer i2) {
+                return Integer.compare(givenArray[i1], givenArray[i2]);
+            }
+        });
+
+
+
+        Arrays.sort(givenArray);
+        int lowerBoundry= 0;
+        int higherBoundry= givenArray.length-1;
+        int resultedIndex=-1;
+
+        while(lowerBoundry<=higherBoundry){
+            int middleIndex=(lowerBoundry+higherBoundry)/2;
+            if (givenArray[middleIndex]<=elementToFindIndex) {
+                lowerBoundry=middleIndex+1;
+                if (givenArray[middleIndex]==elementToFindIndex)
+                    return indices[middleIndex];
+            }
+            else higherBoundry=middleIndex-1;
+        }
+        return resultedIndex;
+    }
 }
--- src/test/java/de/iav/BinarySearchTest.java
+++ src/test/java/de/iav/BinarySearchTest.java
@@ -2,6 +2,32 @@ package de.iav;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class BinarySearchTest {
 
+    @Test
+    public void byGiving_3_ToTheMethod_Index_2_WillBeReturned(){
+        int[] givenArray = new int[]{5, 7, 3, 1, 9, 0};
+        int elementToFind= 3;
+
+        int expectedIndex = 2;
+        int resultedIndex=BinarySearch.searchIndexOfAnArray(givenArray, elementToFind);
+
+
+        Assertions.assertEquals( expectedIndex, resultedIndex );
+    }
+
+    @Test
+    public void byGiving_11_ToTheMethod_Index_MinusOne_WillBeReturned(){
+        int[] givenArray = new int[]{5, 7, 3, 1, 9, 0};
+        int elementToFind= 11;
+
+        int expectedIndex = -1;
+        int resultedIndex=BinarySearch.searchIndexOfAnArray(givenArray, elementToFind);
+
+
+        Assertions.assertEquals( expectedIndex, resultedIndex );
+    }
 }
